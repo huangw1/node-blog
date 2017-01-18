a52bc3f4155fa671508501ba37dbc01c26d28251
unknown <wlhuang@bizcn.net>
2017-01-18 17:39:56 +0800

    handle index animation.

--- public/js/index.js
+++ public/js/index.js
@@ -8,24 +8,7 @@ function systole(){
 		return
 	}
 	var wrapper = $('.history-date'),
-		button = wrapper.find('h2 a'),
-		parentH,
-		elements = []
-	
-	parentH = wrapper.parent().height()
-	
-	setTimeout(function(){
-		
-		wrapper.find('ul').children(":not('h2:first')").each(function(id){
-			elements.push($(this).position().top)
-			$(this).css({'margin-top':-elements[id]}).children().hide()
-		}).animate({'margin-top':0}, 0).children().fadeIn()
-
-		wrapper.parent().animate({"height":parentH}, 0)
-
-		wrapper.find('ul').children(":not('h2:first')").addClass('bounceInDown').css({'-webkit-animation-duration':'0','-webkit-animation-delay':'0','-webkit-animation-timing-function':'ease','-webkit-animation-fill-mode':'both'}).end().children('h2').css({'position':'relative'})
-		
-	}, 0)
+		button = wrapper.find('h2 a')
 
 	button.click(function(){
 		$(this).parent().css({'position':'relative'})
